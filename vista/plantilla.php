ed48fe1c7bb58cdd014357fb3f649dd4e59ebd90
andrettiMo <famolano18@misena.edu.co>
2021-07-26 10:27:47 -0500

    Se configuro el navbar

---
+++ vista/plantilla.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once "vista/modulo/cabecera.php";
+
+if (isset($_GET["ruta"])) {
+   if ($_GET["ruta"]== "inicio"||
+       $_GET["ruta"]== "donante"||
+       $_GET["ruta"]== "recoleccion"||
+       $_GET["ruta"]== "enfermero"||
+       $_GET["ruta"]== "informes") {
+     
+        include_once "vista/modulo/".$_GET["ruta"].".php";
+   }
+} else {
+   
+}
+
+
+include_once "vista/modulo/pie.php";
